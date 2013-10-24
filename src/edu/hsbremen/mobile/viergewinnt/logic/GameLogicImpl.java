e454230fe1a23bb4803b3b6116ba268eb62aa85f
TKnuck <tknuck@stud.hs-bremen.de>
2013-10-24 15:54:21 +0200

    Implemented token placement.

--- src/edu/hsbremen/mobile/viergewinnt/logic/GameLogicImpl.java
+++ src/edu/hsbremen/mobile/viergewinnt/logic/GameLogicImpl.java
@@ -2,34 +2,53 @@ package edu.hsbremen.mobile.viergewinnt.logic;
 
 public class GameLogicImpl implements GameLogic {
 
+	private GameState state;
+	private Gamefield gamefield;
+	private Token currentToken; 
+	
+	public final int WIDTH = 7;
+	public final int HEIGHT = 6;
+	
+	public GameLogicImpl()
+	{
+		state = GameState.INITIALIZED;
+		gamefield = new Gamefield(WIDTH,HEIGHT);
+		currentToken = Token.Red; //red begins
+	}
+	
 	@Override
 	public void startGame() {
-		// TODO Auto-generated method stub
-		
+		state = GameState.RUNNING;
 	}
 
 	@Override
 	public GameState getGameState() {
-		// TODO Auto-generated method stub
-		return null;
+		return state;
 	}
 
 	@Override
 	public void placeToken(int row) {
-		// TODO Auto-generated method stub
-		
+		gamefield.placeToken(row, currentToken);
+		changeToken();
 	}
 
 	@Override
 	public Token[][] getGamefield() {
-		// TODO Auto-generated method stub
-		return null;
+		return gamefield.getField();
 	}
 
 	@Override
 	public Token getWinner() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	private void changeToken()
+	{
+		if (currentToken.equals(Token.Red))
+			currentToken = Token.Blue;
+		else
+			currentToken = Token.Red;
 	}
 
 }
---
+++ src/edu/hsbremen/mobile/viergewinnt/logic/Gamefield.java
@@ -0,0 +1,71 @@
+package edu.hsbremen.mobile.viergewinnt.logic;
+
+//Represents the gamefield.
+//Is able to place tokens and check for a winner.
+/**
+ * Represents the gamefield.
+ * Is able to place tokens and check for a winner.
+ * @author Thorsten
+ *
+ */
+public class Gamefield {
+	
+	private Token[][] field;
+	private int lastMove = 0;
+	
+	/**
+	 * Creates the gamefield.
+	 * @param width Width of the gamefield.
+	 * @param height Height of the gamefield.
+	 */
+	public Gamefield(int width, int height) {
+		field = new Token[width][height];
+		
+		//initialize fields with None Tokens.
+		for (int x = 0; x < width; x++)
+		{
+			for (int y = 0; y < height; y++)
+			{
+				field[x][y] = Token.None;
+			}
+		}
+	}
+	
+	public Token[][] getField()
+	{
+		return field;
+	}
+	
+	/***
+	 * Places a token into the given row on the gamefield.
+	 * @param row
+	 * @param token the Token that has to be placed.
+	 * @return
+	 */
+	public void placeToken(int row, Token token)
+	{
+		int height = getRowHeight(row);
+		field[row][height] = token;
+	}
+	
+	/***
+	 * Returns the current amount of tokens in a specific row.
+	 * @param row
+	 * @return
+	 */
+	private int getRowHeight(int row)
+	{
+		int height = -1;
+		Token currentToken;
+		
+		do 
+		{
+			height++;
+			currentToken = field[row][height];
+		}
+		while (!currentToken.equals(Token.None));
+			
+		return height;
+	}
+	
+}
