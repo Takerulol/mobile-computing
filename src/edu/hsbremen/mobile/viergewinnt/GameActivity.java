af9b48f90f12c859b006912cfe82b05d48082e1b
Takeru <cbollmann@stud.hs-bremen.de>
2013-10-28 15:51:28 +0100

    Simple app with activity switching

--- src/edu/hsbremen/mobile/viergewinnt/GameActivity.java
+++ src/edu/hsbremen/mobile/viergewinnt/GameActivity.java
@@ -4,11 +4,14 @@ import edu.hsbremen.mobile.viergewinnt.util.SystemUiHider;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -112,6 +115,21 @@ public class GameActivity extends Activity {
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
         findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+
+        Button button = (Button)findViewById(R.id.button1);
+        button.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//Starting a new Intent
+//                Intent nextScreen = new Intent(getApplicationContext(), FieldActivity.class);
+ 
+                //Sending data to another Activity
+//                nextScreen.putExtra("name", inputName.getText().toString());
+//                nextScreen.putExtra("email", inputEmail.getText().toString());
+ 
+//                startActivity(nextScreen);			
+			}
+		});
     }
 
     @Override
