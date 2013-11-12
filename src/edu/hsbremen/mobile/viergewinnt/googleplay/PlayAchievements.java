615d6cf1716253761b075f09de8dfdb647a24286
TKnuck <tknuck@stud.hs-bremen.de>
2013-11-12 11:32:30 +0100

    Added ids.xml

---
+++ src/edu/hsbremen/mobile/viergewinnt/googleplay/PlayAchievements.java
@@ -0,0 +1,25 @@
+package edu.hsbremen.mobile.viergewinnt.googleplay;
+
+import java.util.EnumSet;
+
+import edu.hsbremen.mobile.viergewinnt.logic.Achievement;
+import edu.hsbremen.mobile.viergewinnt.logic.AchievementLogic;
+
+public class PlayAchievements implements AchievementLogic {
+
+	
+	public PlayAchievements() {}
+	
+	@Override
+	public void unlockAchievement(Achievement achivement) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public EnumSet<Achievement> getUnlockedAchievements() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
